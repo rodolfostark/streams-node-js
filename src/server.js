7c17a5dc9c5c7dd5f623097bf10f3ebea93ca008
rodolfostark <rodolfojonatas@outlook.com>
2024-02-26 22:16:22 -0300

    build: setup response headers

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((request, response) => {
     const { method, url } = request
 
-    console.log(method, url)
+    if (method === 'GET' && url === '/users') {
+        return response
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
+    }
+
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@example.com'
+        })
+    }
 
     return response.end('Hello World')
 })
